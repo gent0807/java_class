c84329c5270b87fe1fda2e4ac80e9f881dc39400
gent0807 <theloopholesnk@gmail.com>
2022-03-18 02:31:49 +0900

    dd

--- src/Package/TestClass.java
+++ src/Package/TestClass.java
@@ -6,6 +6,7 @@ public class TestClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello! This is Github Practicing!");//Github Practice!
 		System.out.println("Hello! This is Github Practicing!");//Github Practice!
+		System.out.println("Hello! This is Github Practicing!");//Github Practice!
 
 	}
 
